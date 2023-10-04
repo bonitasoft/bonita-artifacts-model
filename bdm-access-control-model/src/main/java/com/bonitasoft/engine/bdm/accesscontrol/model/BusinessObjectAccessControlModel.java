23d47d2da1e17e9c2e60b8a803094b845c7532dd
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-10-04 11:51:21 +0200

    fix(bdm): Do not set a default productVersion
    The `productVersion` attribute is not relevant.
    
    This attribute will now be used at runtime when generating the java jar
    to identify the productVersion used for generation

--- bdm-access-control-model/src/main/java/com/bonitasoft/engine/bdm/accesscontrol/model/BusinessObjectAccessControlModel.java
+++ bdm-access-control-model/src/main/java/com/bonitasoft/engine/bdm/accesscontrol/model/BusinessObjectAccessControlModel.java
@@ -13,15 +13,9 @@
  **/
 package com.bonitasoft.engine.bdm.accesscontrol.model;
 
-import static org.bonitasoft.engine.bdm.model.BusinessObjectModel.INFO_PROPERTIES_RESOURCE;
-import static org.bonitasoft.engine.bdm.model.BusinessObjectModel.VERSION_PROPERTY_KEY;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.Properties;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -29,10 +23,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-
-import org.glassfish.hk2.osgiresourcelocator.ResourceFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Adrien Lachambre, Emmanuel Duchastenier
@@ -43,25 +33,6 @@ import org.slf4j.LoggerFactory;
 public class BusinessObjectAccessControlModel {
 
     public static final String CURRENT_MODEL_VERSION = "1.0";
-
-    public static final String CURRENT_PRODUCT_VERSION;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(BusinessObjectAccessControlModel.class);
-
-    static {
-        final Properties info = new Properties();
-        try {
-            var infoProperties = Optional.ofNullable(ResourceFinder.findEntry(INFO_PROPERTIES_RESOURCE))
-                    .orElseGet(() -> BusinessObjectAccessControlModel.class.getResource(INFO_PROPERTIES_RESOURCE));
-            if (infoProperties != null) {
-                info.load(infoProperties.openStream());
-            }
-        } catch (final IOException e) {
-            LOGGER.error("Failed to retrieve product version", e);
-        }
-        final String version = info.getProperty(VERSION_PROPERTY_KEY);
-        CURRENT_PRODUCT_VERSION = version == null || version.isBlank() ? null : version;
-    }
 
     @XmlAttribute(name = "modelVersion")
     private String modelVersion;
